bf39a32f03035159b1751f8576907a9d9809749e
AnnaVaranko <avoronk@yandex.ru>
2022-02-23 17:55:58 +0300

    output of variables on screen

---
+++ Task3_11/Task3.java
@@ -0,0 +1,23 @@
+package Task3_11;
+
+public class Task3 {
+    public static void main(String[] args){
+        byte b = 0X55;
+        short s = 0x55ff;
+        int i = 1_000_000;
+        long l = 0xfff_fff_ffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .000_012_34;
+        boolean bool = true;
+
+        System.out.println("byte b = " + b);
+        System.out.println("short s = " + s);
+        System.out.println("int i = " + i);
+        System.out.println("long l = " + l);
+        System.out.println("char c = " + c);
+        System.out.println("float f = " + f);
+        System.out.println("double d = " + d);
+        System.out.println("boolean bool = " + bool);
+    }
+}
