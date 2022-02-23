a611623cfdf36eb5cfb10b236d6e70923f83fd74
AnnaVaranko <avoronk@yandex.ru>
2022-02-23 17:30:20 +0300

    Output Hello Belarus + I like Java

--- Task1_2_3/Task1_2_3.java
+++ Task1_2_3/Task1_2_3.java
@@ -3,5 +3,6 @@ package Task1_2_3;
 public class Task1_2_3 {
     public static void main(String[] args){
         System.out.println("Hello Belarus!");
+        System.out.println("I like Java!");
     }
 }
