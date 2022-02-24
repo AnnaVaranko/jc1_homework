25609d98256326a0737fbcf19932dbebcdca4848
AnnaVaranko <avoronk@yandex.ru>
2022-02-24 15:42:28 +0300

    Is the number ending at seven: return boolean(true or false)

---
+++ Task3_11/Task6.java
@@ -0,0 +1,13 @@
+package Task3_11;
+
+public class Task6 {
+    int number;
+    public boolean getBoolean (int number){
+        this.number = number%10;
+        return this.number == 7;
+
+    }
+    public static void main(String[] args){
+        System.out.println(new Task6().getBoolean(125));
+    }
+}
