2de121d7984c162fe14de423f6060a74fa4c8567
AnnaVaranko <avoronk@yandex.ru>
2022-02-24 19:28:38 +0300

    Commit in class

--- Task1_2_3/Task1_2_3.java
+++ Task1_2_3/Task1_2_3.java
@@ -4,5 +4,6 @@ public class Task1_2_3 {
     public static void main(String[] args){
         System.out.println("Hello Belarus!");
         System.out.println("I like Java!");
+        System.out.println("I like Java!");
     }
 }
