b48b2e4f67040fb2790ccc7db14a5ffa69edfa6f
AnnaVaranko <avoronk@yandex.ru>
2022-04-07 12:46:29 +0300

    Created array list with student's note

---
+++ Task39_45/Task39/Task39.java
@@ -0,0 +1,10 @@
+package Task39_45.Task39;
+
+public class Task39 {
+    public static void main(String[] args) {
+
+        CreateArrayList createArrayList = new CreateArrayList();
+        createArrayList.writeArrayList();
+
+    }
+}
