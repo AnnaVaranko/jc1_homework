b8c571375997f7c540f7c9bac951e7c23f39753e
AnnaVaranko <avoronk@yandex.ru>
2022-05-01 22:56:06 +0300

    Thread

---
+++ task46_54/task53/task53.java
@@ -0,0 +1,44 @@
+package task46_54.task53;
+
+public class task53 {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 1; i < 11; i++) {
+            MyThread myThread = new MyThread();
+            myThread.setName("Thread" + i);
+            System.out.println(myThread.getName());
+            myThread.start();
+            Thread.sleep(1_000);
+        }
+    }
+}
+
+class MyThread extends Thread{
+
+    private int[] arr = new int[10];
+
+    @Override
+    public void run() {
+        getArray();
+        System.out.println();
+        System.out.println("Max number: " + searchMaxInt());
+    }
+
+    private synchronized void getArray(){
+        for(int i = 0; i < 10; i++){
+            arr[i] = (int) (Math.random() * 20 + 1);
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private synchronized int searchMaxInt(){
+        int firstNumber = arr[0];
+
+        for(int i = 1; i < 10; i++){
+            if(firstNumber < arr[i]){
+                firstNumber = arr[i];
+            }
+        }
+        return firstNumber;
+    }
+}
